f0a597412372417d272845a3413ab283af9f18e7
mollnn <1084918073@qq.com>
2021-05-19 21:21:46 +0800

    feat (12): stream

--- src/12_texturebuffer/12_texturebuffer.cpp
+++ src/12_texturebuffer/12_texturebuffer.cpp
@@ -6,6 +6,19 @@
 #include "Shader.h"
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 const GLuint WIDTH = 1600, HEIGHT = 900;
+
+void CopyFromFramebufferToTexture(GLuint fboSrc, GLuint texDest, int xo, int yo, int x, int y, int w, int h)
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, fboSrc);
+
+    glBindTexture(GL_TEXTURE_2D, texDest);
+
+    glCopyTexSubImage2D(GL_TEXTURE_2D, 0, xo, yo, x, y, w, h);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
 int main()
 {
     glfwInit();
@@ -52,9 +65,13 @@ int main()
 
     glBindVertexArray(0); // Unbind VAO
 
-    GLuint texture1;
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);                       // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+    // textureSource 为新图像的来源
+    // textureLast   为上次的结果
+    // textureResult 为本次合成的图像
+
+    GLuint textureSource;
+    glGenTextures(1, &textureSource);
+    glBindTexture(GL_TEXTURE_2D, textureSource);                  // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Set texture wrapping to GL_REPEAT
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -66,10 +83,9 @@ int main()
     SOIL_free_image_data(image);
     glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
 
-
-    GLuint texture2;
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);                       // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+    GLuint textureLast;
+    glGenTextures(1, &textureLast);
+    glBindTexture(GL_TEXTURE_2D, textureLast);                    // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Set texture wrapping to GL_REPEAT
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -77,36 +93,68 @@ int main()
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
 
-    // ----------------------------------------------------------
-    // create a new frame buffer and do some stupid image copy
-    // ----------------------------------------------------------
+    GLuint textureResult;
+    glGenTextures(1, &textureResult);
+    glBindTexture(GL_TEXTURE_2D, textureResult);                  // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Set texture wrapping to GL_REPEAT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
 
-    GLuint fboId;
-    glGenFramebuffers(1, &fboId);
-    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture1, 0);
+    GLuint textureEmpty;
+    glGenTextures(1, &textureEmpty);
+    glBindTexture(GL_TEXTURE_2D, textureEmpty);                   // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Set texture wrapping to GL_REPEAT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
 
-    GLuint outTexId=texture2;
+    GLuint fboSource;
+    glGenFramebuffers(1, &fboSource);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboSource);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureSource, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    // fboId: the from FBO id
-    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+    GLuint fboLast;
+    glGenFramebuffers(1, &fboLast);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboLast);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureLast, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    // outTexId: the target FBO id
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, outTexId);
+    GLuint fboResult;
+    glGenFramebuffers(1, &fboResult);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboResult);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureResult, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0,
-                        0, 0, width, height);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
+    GLuint fboEmpty;
+    glGenFramebuffers(1, &fboEmpty);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboEmpty);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureEmpty, 0);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     int cnt = 0;
 
     while (!glfwWindowShouldClose(window))
     {
-
         ++cnt;
+
+        // ----------------------------------------------------------
+        // create a new frame buffer and do some stupid image copy
+        // ----------------------------------------------------------
+
+        int w0 = 2;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            CopyFromFramebufferToTexture(fboResult, textureLast, 0, 0, 0, 0, width, height);
+            CopyFromFramebufferToTexture(fboLast, textureResult, 0, 0, w0 - 1, 0, width - w0 + 1, height);
+            CopyFromFramebufferToTexture(cnt % 2 ? fboSource : fboEmpty, textureResult, width - w0, 0, 0, 0, w0, height);
+        }
 
         glfwPollEvents();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -115,9 +163,8 @@ int main()
         ourShader.Use();
 
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture2);
+        glBindTexture(GL_TEXTURE_2D, textureResult);
         glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
-        glActiveTexture(GL_TEXTURE1);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
